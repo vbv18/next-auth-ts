d3a1855e977501856202b4aec5711445ed5255b1
Vaibhav Garg <23uec638@lnmiit.ac.in>
2026-06-10 01:44:30 +0530

    configured envSchema

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,7 +3,29 @@ import { z } from "zod";
 
 
 const ENVSchema = z.object({
+    NODE_ENV: z.enum(["development", "production", "test"]).default("development"),
+    AUTH_URL: z.url(),
+    AUTH_SECRET: z.string().min(1),
 
+    MONGODB_URI: z.string().min(1),
+
+    SALT_ROUNDS: z.coerce.number().int().positive(),
+
+    VERIFICATION_TOKEN_TTL: z.string().regex(/^\d+[mhd]$/),
+    PASSWORD_RESET_TOKEN_TTL: z.string().regex(/^\d+[mhd]$/),
+
+    SMTP_HOST: z.string().min(1),
+    SMTP_PORT: z.coerce.number().int().min(1).max(65535).default(587),
+    SMTP_SECURE: z.coerce.boolean().default(false),
+    SMTP_USER: z.string().min(1),
+    SMTP_PASSWORD: z.string().min(1),
+    SMTP_FROM: z.email(),
+
+    GOOGLE_CLIENT_ID: z.string().optional(),
+    GOOGLE_CLIENT_SECRET: z.string().optional(),
+
+    GITHUB_CLIENT_ID: z.string().optional(),
+    GITHUB_CLIENT_SECRET: z.string().optional()
 });
 
 const parsedENV = ENVSchema.safeParse(process.env);
