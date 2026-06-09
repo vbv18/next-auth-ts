5a6c765a4d8f19794f7a1fc72236110334e71364
Vaibhav Garg <23uec638@lnmiit.ac.in>
2026-06-09 15:27:14 +0530

    defined user schema

---
+++ src/models/user.model.ts
@@ -0,0 +1,68 @@
+import mongoose, { Schema, Document, Model } from "mongoose";
+
+export interface IUser extends Document {
+    username: string,
+    email: string,
+    password?: string,
+    image?: string,
+    role: "user" | "admin",
+    provider: "credentials" | "google" | "github",
+    emailVerified: boolean,
+
+    createdAt: Date,
+    updatedAt: Date
+}
+
+const UserSchema = new Schema<IUser>(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true
+        },
+
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+            index: true
+        },
+
+        password: {
+            type: String,
+            select: false
+        },
+
+        image: {
+            type: String,
+            default: null
+        },
+
+        role: {
+            type: String,
+            enum: ["user", "admin"],
+            default: "user"
+        },
+
+        provider: {
+            type: String,
+            enum: ["credentials", "google", "github"],
+            required: true,
+            default: "credentials",
+        },
+
+        emailVerified: {
+            type: Boolean,
+            default: false,
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+
+export const User: Model<IUser> = mongoose.models.User || mongoose.model<IUser>("User", UserSchema);
