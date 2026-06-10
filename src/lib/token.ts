deee0793a30828a04520cd924cfaab7f91cde9a4
Vaibhav Garg <23uec638@lnmiit.ac.in>
2026-06-10 13:52:02 +0530

    add tokens' TTL_MS using ms lib

--- src/lib/token.ts
+++ src/lib/token.ts
@@ -1,5 +1,9 @@
 import crypto from "crypto";
+import ms, { type StringValue } from "ms";
+import { ENV } from "@/config/config";
 
+export const VERIFICATION_TOKEN_TTL_MS = ms(ENV.VERIFICATION_TOKEN_TTL as StringValue);
+export const PASSWORD_RESET_TOKEN_TTL_MS = ms(ENV.PASSWORD_RESET_TOKEN_TTL as StringValue);
 
 export function generateToken() {
     const token = crypto.randomUUID();
