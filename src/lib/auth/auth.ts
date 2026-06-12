025b41a77cefaac792b47bad6692c825ed057c31
Vaibhav Garg <23uec638@lnmiit.ac.in>
2026-06-13 00:59:46 +0530

    add session, callback, pages

--- src/lib/auth/auth.ts
+++ src/lib/auth/auth.ts
@@ -1,9 +1,65 @@
-import CredentialProvider from "next-auth/providers/credentials";
+import type { NextAuthConfig } from "next-auth";
+import Credentials from "next-auth/providers/credentials";
 
-export let authOptions = {
+import { LoginSchema } from "../validations/auth";
+import { loginUser } from "@/lib/services/auth.service";
+import { ENV } from "@/config/config";
+
+
+export const authOptions = {
+    secret: ENV.AUTH_SECRET,
+
+    trustHost: true,
+
+    pages: {
+        signIn: "/login"
+    },
+
+    session: {
+        strategy: "jwt"
+    },
+
+    callbacks: {
+        async jwt({ token, user }) {
+            if (user) {
+                token.id = user.id;
+                token.role = user.role;
+                token.username = user.username;
+            }
+
+            return token;
+        },
+
+        async session({ session, token }) {
+            if (session.user) {
+                session.user.id = token.id as string;
+                session.user.role = token.role as "user" | "admin";
+                session.user.username = token.username as string;
+            }
+
+            return session;
+        }
+    },
+
     providers: [
-        CredentialProvider({
-            name: "credentials"
+        Credentials({
+            name: "credentials",
+            credentials: {
+                email: {},
+                password: {},
+            },
+
+            async authorize(credentials) {
+                const parsed = LoginSchema.safeParse(credentials);
+
+                if (!parsed.success) {
+                    return null;
+                }
+
+                const { email, password } = parsed.data;
+
+                return loginUser(email, password);
+            },
         }),
-    ]
-};
+    ],
+} satisfies NextAuthConfig;
